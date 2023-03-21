d361eed2ddea7361d347d9ada2ee0164094a2792
Jackline-ke <antoninahodhiambo@gmail.com>
2023-03-21 15:59:32 +0300

    Toy Problem

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function grade(){
+    let text = document.getElementById("input").value;
+    if(text > 79){
+        text = "Grade: A";
+    }else if(text >= 60 && text < 80){
+        text = "Grade: B";
+    }else if(text <= 59 && text >=49){
+        text = "Grade: C";
+    }else if(text >=40 && text <=48){
+        text = "Grade: D";
+    }else{
+        text = "Grade: E";
+    }
+
+alert(text);
+}
+grade();
+
+
+
+
+
